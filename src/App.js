4020945c14455f0c3b8dedee0d43e2e0e80ecc0f
Shuvam Kumar Panda <shuvamkumar2001@gmail.com>
2022-10-15 03:11:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-5xl font-bold underline">
+      Mool Website 2.0
+    </div>
+  );
+}
+
+export default App;
